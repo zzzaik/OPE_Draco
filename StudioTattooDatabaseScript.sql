96ba8793af5d39d4da6665cb2ee55087e3944976
Gustavo Zaik <zzzaik21@gmail.com>
2018-09-03 16:12:03 +0000

    Script Funcionanado no mySQL

--- StudioTattooDatabaseScript.sql
+++ StudioTattooDatabaseScript.sql
@@ -1,97 +1,97 @@
 create table Tatuador (
-	idTatuador AUTO_INCREMENT,
-	nomeTatuador VARCHAR,
-	loginTatuador VARCHAR,
-	senhaTatuador VARCHAR,
-	apiTokens VARCHAR NULL,
+	idTatuador INT AUTO_INCREMENT,
+	nomeTatuador VARCHAR(120),
+	loginTatuador VARCHAR(100),
+	senhaTatuador VARCHAR(300),
+	apiTokens VARCHAR(350) NULL,
 	CONSTRAINT pk_Tatuador PRIMARY KEY (idTatuador)
-)
+);
 
 create table Cliente (
-	idCliente AUTO_INCREMENT,
-	nomeCliente VARCHAR,
-	loginCliente VARCHAR,
-	senhaCliente VARCHAR,
+	idCliente INT AUTO_INCREMENT,
+	nomeCliente VARCHAR(120),
+	loginCliente VARCHAR(100),
+	senhaCliente VARCHAR(300),
 	assiduidade BOOL NULL,
 	CONSTRAINT pk_Cliente PRIMARY KEY (idCliente)
-)
+);
 
 
 create table Agenda (
-	idAgenda AUTO_INCREMENT,
+	idAgenda INT AUTO_INCREMENT,
 	dataAgenda DATE,
 	horarioAgenda TIME,
 	CONSTRAINT pk_Agenda PRIMARY KEY (idAgenda)
-)
+);
 
 create table Estilos (
-	idEstilo AUTO_INCREMENT,
-	estilo VARCHAR,
+	idEstilo INT AUTO_INCREMENT,
+	estilo VARCHAR(50),
 	CONSTRAINT pk_Estilos PRIMARY KEY (idEstilo)
-)
+);
 
 create table Regiao(
-	idRegiao AUTO_INCREMENT,
-	regiaoDoCorpo VARCHAR,
+	idRegiao INT AUTO_INCREMENT,
+	regiaoDoCorpo VARCHAR(50),
 	faixaValor REAL,
 	CONSTRAINT pk_Regiao PRIMARY KEY (idRegiao)
-)
+);
 
 create table Cor (
-	idCor AUTO_INCREMENT,
+	idCor INT AUTO_INCREMENT,
 	colorido BOOL,
 	faixaValor REAL,
 	CONSTRAINT pk_Cor PRIMARY KEY (idCor)
-)
+);
 
 create table Tamanho (
-	idTamanho AUTO_INCREMENT,
-	tamanho VARCHAR,
+	idTamanho INT AUTO_INCREMENT,
+	tamanho VARCHAR(8),
 	faixaValor REAL,
 	CONSTRAINT pk_Tamanho PRIMARY KEY (idTamanho)
-)
+);
 
 create table TelefoneCliente (
-	idTelefone AUTO_INCREMENT,
+	idTelefone INT AUTO_INCREMENT,
 	idCliente INT,
-	numeroTelefone VARCHAR,
+	numeroTelefone VARCHAR(15),
 	CONSTRAINT pk_TelefoneCliente PRIMARY KEY (idTelefone),
 	CONSTRAINT fk_Cliente FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente)
-)
+);
 
 create table EmailCliente (
-	idEmail AUTO_INCREMENT,
+	idEmail INT AUTO_INCREMENT,
 	idCliente INT,
-	enderecoEmail VARCHAR,
+	enderecoEmail VARCHAR(100),
 	CONSTRAINT pk_EmailCliente PRIMARY KEY (idEmail),
 	CONSTRAINT fk_Cliente FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente)
-)
+);
 
 create table Imagens (
-	idImagem AUTO_INCREMENT,
-	urlImagem VARCHAR,
+	idImagem INT AUTO_INCREMENT,
+	urlImagem VARCHAR(350),
 	ratins INT,
 	idEstilo INT,
 	CONSTRAINT pk_Imagem PRIMARY KEY (idImagem),
 	CONSTRAINT fk_Estilo FOREIGN KEY (idEstilo) REFERENCES Estilos(idEstilo)
-)
+);
 
 create table Portifolio (
-	idPortifolio AUTO_INCREMENT,
+	idPortifolio INT AUTO_INCREMENT,
 	idImagem INT,
 	CONSTRAINT pk_Portifolio PRIMARY KEY (idPortifolio),
 	CONSTRAINT fk_ImagemPortifolio FOREIGN KEY (idImagem) REFERENCES Imagens(idImagem)
-)
+);
 
 create table Catalogo (
-	idCatalogo AUTO_INCREMENT,
+	idCatalogo INT AUTO_INCREMENT,
 	idImagem INT,
 	CONSTRAINT pk_Catalogo PRIMARY KEY (idCatalogo),
 	CONSTRAINT fk_ImagemCatalogo FOREIGN KEY (idImagem) REFERENCES Imagens(idImagem)
-)
+);
 
 create table Servicos (
-	idServico AUTO_INCREMENT,
+	idServico INT AUTO_INCREMENT,
 	idCliente INT,
 	idImagem INT,
 	idCor INT,
@@ -105,15 +105,124 @@ create table Servicos (
 	CONSTRAINT fk_CorServico FOREIGN KEY (idCor) REFERENCES Cor(idCor),
 	CONSTRAINT fk_TamanhoServico FOREIGN KEY (idTamanho) REFERENCES Tamanho(idTamanho),
 	CONSTRAINT fk_RegiaoServico FOREIGN KEY (idRegiao) REFERENCES Regiao(idRegiao)
-)
+);
 
 create table Sessao (
-	idSessao AUTO_INCREMENT,
+	idSessao INT AUTO_INCREMENT,
 	idAgenda INT,
 	idServico INT,
 	CONSTRAINT pk_Sessao PRIMARY KEY (idSessao),
 	CONSTRAINT fk_AgendaSessao FOREIGN KEY (idAgenda) REFERENCES Agenda(idAgenda),
 	CONSTRAINT fk_ServicoSessao FOREIGN KEY (idServico) REFERENCES Servicos(idServico)
-)
+);mysql> status
+--------------
 
+Connection id:		64194996
+Current database:	zzzaik$JustinoTattooDB
+Current user:		zzzaik@10.0.0.4
+SSL:			Cipher in use is AES256-SHA
+Current pager:		stdout
+Using outfile:		'/home/zzzaik/OPE_Draco/StudioTattooDatabaseScript.sql'
+Using delimiter:	;
+Server version:		5.6.27-log MySQL Community Server (GPL)
+Protocol version:	10
+Connection:		zzzaik.mysql.pythonanywhere-services.com via TCP/IP
+Server characterset:	utf8
+Db     characterset:	utf8
+Client characterset:	utf8
+Conn.  characterset:	utf8
+TCP port:		3306
+Uptime:			257 days 58 min 55 sec
 
+Threads: 106  Questions: 4267765010  Slow queries: 34643  Opens: 24788501  Flush tables: 1  Open tables: 992  Queries per second avg: 192.169
+--------------
+
+mysql> print
+--------------
+
+--------------
+
+mysql> ego
+ERROR: 
+No query specified
+
+mysql> clear
+mysql> status
+--------------
+
+Connection id:		64194996
+Current database:	zzzaik$JustinoTattooDB
+Current user:		zzzaik@10.0.0.4
+SSL:			Cipher in use is AES256-SHA
+Current pager:		stdout
+Using outfile:		'/home/zzzaik/OPE_Draco/StudioTattooDatabaseScript.sql'
+Using delimiter:	;
+Server version:		5.6.27-log MySQL Community Server (GPL)
+Protocol version:	10
+Connection:		zzzaik.mysql.pythonanywhere-services.com via TCP/IP
+Server characterset:	utf8
+Db     characterset:	utf8
+Client characterset:	utf8
+Conn.  characterset:	utf8
+TCP port:		3306
+Uptime:			257 days 59 min 49 sec
+
+Threads: 105  Questions: 4267809558  Slow queries: 34643  Opens: 24788595  Flush tables: 1  Open tables: 992  Queries per second avg: 192.171
+--------------
+
+mysql> print ego
+    -> StudioTattooDatabaseScript.sql
+    -> ^C
+mysql> print
+--------------
+
+--------------
+
+mysql> \h
+
+For information about MySQL products and services, visit:
+   http://www.mysql.com/
+For developer information, including the MySQL Reference Manual, visit:
+   http://dev.mysql.com/
+To buy MySQL Enterprise support, training, or other products, visit:
+   https://shop.mysql.com/
+
+List of all MySQL commands:
+Note that all text commands must be first on line and end with ';'
+?         (\?) Synonym for `help'.
+clear     (\c) Clear the current input statement.
+connect   (\r) Reconnect to the server. Optional arguments are db and host.
+delimiter (\d) Set statement delimiter.
+edit      (\e) Edit command with $EDITOR.
+ego       (\G) Send command to mysql server, display result vertically.
+exit      (\q) Exit mysql. Same as quit.
+go        (\g) Send command to mysql server.
+help      (\h) Display this help.
+nopager   (\n) Disable pager, print to stdout.
+notee     (\t) Don't write into outfile.
+pager     (\P) Set PAGER [to_pager]. Print the query results via PAGER.
+print     (\p) Print current command.
+prompt    (\R) Change your mysql prompt.
+quit      (\q) Quit mysql.
+rehash    (\#) Rebuild completion hash.
+source    (\.) Execute an SQL script file. Takes a file name as an argument.
+status    (\s) Get status information from the server.
+system    (\!) Execute a system shell command.
+tee       (\T) Set outfile [to_outfile]. Append everything into given outfile.
+use       (\u) Use another database. Takes database name as argument.
+charset   (\C) Switch to another charset. Might be needed for processing binlog with multi-byte charsets.
+warnings  (\W) Show warnings after every statement.
+nowarning (\w) Don't show warnings after every statement.
+resetconnection(\x) Clean session context.
+
+For server side help, type 'help contents'
+
+mysql> \x
+ERROR 1047 (08S01): Unknown command
+mysql> \X
+ERROR: 
+Unknown command '\X'.
+    -> ^C
+mysql> resetconnection
+ERROR 1047 (08S01): Unknown command
+mysql> notee
