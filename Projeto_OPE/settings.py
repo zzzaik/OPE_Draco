176959dad6a74af5c76ca6226f8eb950e85f8d68
SparkCat23 <jhoustorm@gmail.com>
2018-10-11 16:48:42 -0300

    CORS server configuration

--- Projeto_OPE/settings.py
+++ Projeto_OPE/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 #needed to make async requests with crossDomain
-from corsheaders.defaults import default_methods,default_headers
+from corsheaders import defaults
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -57,8 +57,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_METHODS = default_methods
-CORS_ALLOW_HEADERS = default_headers
+
+CORS_ALLOW_METHODS = defaults.default_methods
+
+CORS_ALLOW_HEADERS = defaults.default_methods
+
 CORS_ALLOW_CREDENTIALS = True
 
 
